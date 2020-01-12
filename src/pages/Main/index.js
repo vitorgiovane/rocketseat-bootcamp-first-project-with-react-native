6085677314e7c16dd58250202d7e0cd1d018986a
Vitor Giovane <vitorgpalves@gmail.com>
2020-01-12 01:40:41 -0300

    feature: adiciona conexão com a api do Github

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,56 @@
-import React from 'react'
-import {Container, Form, Input, SubmitButton} from './styles'
+import React, { Component } from 'react'
+import { Container, Form, Input, SubmitButton } from './styles'
+import api from '../../services/api'
+import { Keyboard } from 'react-native'
 
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Add user"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#fff" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  )
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: []
+  }
+
+  handleAddNewUser = async () => {
+    const { users, newUser } = this.state
+    const response = await api.get(`/users/${newUser}`)
+    const { name, login, bio, avatar_url: avatar } = response.data
+
+    const data = {
+      name,
+      login,
+      bio,
+      avatar
+    }
+
+    this.setState({
+      users: [...users, data],
+      newUser: ''
+    })
+
+    Keyboard.dismiss()
+  }
+
+  render() {
+    const { users, newUser } = this.state
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Add user"
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddNewUser}
+          />
+          <SubmitButton onPress={this.handleAddNewUser}>
+            <Icon name="add" size={20} color="#fff" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    )
+  }
 }
 
 Main.navigationOptions = {
