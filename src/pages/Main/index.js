42ae3ca91a27d81f2c00db30f2f20f023adb6c3f
Vitor Giovane <vitorgpalves@gmail.com>
2020-01-12 15:02:31 -0300

    feature: adiciona loading e salva usuários no storage do celular

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import { Keyboard, ActivityIndicator } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import api from '../../services/api'
+
 import {
   Container,
   Form,
@@ -13,19 +18,40 @@ import {
   ProfileButtonText
 } from './styles'
 
-import { Keyboard } from 'react-native'
-import Icon from 'react-native-vector-icons/MaterialIcons'
-
-import api from '../../services/api'
-
 export default class Main extends Component {
   state = {
     newUser: '',
-    users: []
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users')
+
+    if (users) {
+      this.setState({
+        users: JSON.parse(users)
+      })
+    }
+  }
+
+  async componentDidUpdate(props, prevState) {
+    const { users } = this.state
+
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users))
+    }
   }
 
   handleAddNewUser = async () => {
+    this.setState({ loading: true })
     const { users, newUser } = this.state
+
+    if (!newUser) {
+      this.setState({ loading: false })
+      return
+    }
+
     const response = await api.get(`/users/${newUser}`)
     const { name, login, bio, avatar_url: avatar } = response.data
 
@@ -38,14 +64,15 @@ export default class Main extends Component {
 
     this.setState({
       users: [...users, data],
-      newUser: ''
+      newUser: '',
+      loading: false
     })
 
     Keyboard.dismiss()
   }
 
   render() {
-    const { users, newUser } = this.state
+    const { users, newUser, loading } = this.state
     return (
       <Container>
         <Form>
@@ -58,8 +85,12 @@ export default class Main extends Component {
             onSubmitEditing={this.handleAddNewUser}
             value={this.state.newUser}
           />
-          <SubmitButton onPress={this.handleAddNewUser}>
-            <Icon name="add" size={20} color="#fff" />
+          <SubmitButton onPress={this.handleAddNewUser} loading={loading}>
+            {loading ? (
+              <ActivityIndicator color="#fff" size={20} />
+            ) : (
+              <Icon name="add" size={20} color="#fff" />
+            )}
           </SubmitButton>
         </Form>
         <List
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -32,6 +32,7 @@ export const SubmitButton = styled(RectButton)`
   border-radius: 4px;
   padding: 0 12px;
   margin-left: 10px;
+  opacity: ${props => (props.loading ? 0.7 : 1)};
 `
 
 export const List = styled.FlatList.attrs({
